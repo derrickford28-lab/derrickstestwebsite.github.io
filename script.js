3302fa0ba02fe87a75bf96696a2e1be2f7ff968e
derrickford28-lab <derrick.ford28@students.fraserk12.org>
2026-03-13 11:47:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 
+const products = [
+
+{
+name:"Gaming Mouse",
+price:40
+},
+
+{
+name:"Keyboard",
+price:60
+},
+
+{
+name:"Headset",
+price:80
+}
+
+]
+
+const container = document.getElementById("products")
+
+products.forEach(product => {
+
+const div = document.createElement("div")
+div.className = "product"
+
+div.innerHTML = `
+<h2>${product.name}</h2>
+<p>$${product.price}</p>
+<button>Add to Cart</button>
+`
+
+container.appendChild(div)
+
+})
